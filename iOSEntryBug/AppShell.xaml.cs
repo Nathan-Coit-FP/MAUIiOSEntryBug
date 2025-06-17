5a657bcbb622d16212a507497300e4d402a3e4e1
Nathan Coit <57372463+Nathan-Coit-FP@users.noreply.github.com>
2025-06-17 13:09:05 -0400

    Sample project

---
+++ iOSEntryBug/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace iOSEntryBug
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ iOSEntryBug/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace iOSEntryBug
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var thread = new Thread(() => {
+                while (true)
+                {
+                    Thread.Sleep(1000);
+                    GC.Collect();
+                }
+            });
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private async void OnCounterClicked(object? sender, EventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                stackLayout.Add(new Entry());
+            }
+            await Task.Delay(500); // Let the entries render
+
+            // Simulate busy main thread and give time to background the app
+            Thread.Sleep(5000);
+
+            // iOS allows this code to finish before backgrounding the app and pausing all threads
+            for (int i = 100; i > 0; i--)
+            {
+                var entry = stackLayout[i] as Entry;
+                stackLayout.Remove(entry);
+                entry?.Handler?.DisconnectHandler();
+            }
+
+            // App will be backgrounded now, GC will run immediately when resumed and crash 
+        }
+    }
+}
